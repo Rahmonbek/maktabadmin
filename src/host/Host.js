a57d913d324a8409226aa4a8b38fb172d507fe7e
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-08-01 22:38:27 +0500

    nilufar

--- src/host/Host.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export const url="http://maktab2.herokuapp.com"
-export const id="2"
-export const httpRequest=(config)=>{
-    return(axios({
-        ...config
-    }))
-}
