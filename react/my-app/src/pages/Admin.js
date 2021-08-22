bb662caad191688be57cbdcdc05ee0c6f235d50d
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-08-22 23:54:59 +0500

    nb

--- react/my-app/src/pages/Admin.js
+++ react/my-app/src/pages/Admin.js
@@ -4,9 +4,9 @@ export default class Admin extends Component {
   state={
     school:null,
   }
-  componentDidMount(){
-    axios.get()
-  }
+  // componentDidMount(){
+  //   axios.get()
+  // }
   render() {
     return (
       <div>
