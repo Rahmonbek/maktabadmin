b2ea4d969a39857dc84b6edcb287029d9e1ee9bc
Developer <62201299+Developer06092000@users.noreply.github.com>
2021-08-24 14:31:29 +0500

    ddsada
    dasdas

--- react/my-app/src/pages/Togaraklar.js
+++ react/my-app/src/pages/Togaraklar.js
@@ -5,140 +5,210 @@ import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import Modal from "antd/lib/modal/Modal";
 import admin from "../img/user.png";
-import { getCourses } from "../host/Config";
+import { createCourses, deleteCourse, editCourse, getCourses, getMentors, getStaff } from "../host/Config";
 import Loader from "./Loader";
+import Global from "../host/Global";
+import { message, Select } from "antd";
+import { Option } from "antd/lib/mentions";
+import ImageDemo from "./ImageDemo";
 export default class Togaraklar extends Component {
   state = {
-    loading:true,
-    teachers: [
-      {
-        rasm: "https://randomuser.me/api/portraits/men/3.jpg",
-        name: "Zohidova Odina",
-        lavozim: "Pianinachi",
-        matn: "O'zbekiston Respublikasi chempioni,Yoshlar orasida yetakchi murabbiy",
-        email: "odina@gmail.com",
-        telefon: "+99923623565",
-      },
-      {
-        rasm: "https://randomuser.me/api/portraits/men/3.jpg",
-        name: "Raximov Asilbek",
-        lavozim: "Gitara ustasi",
-        matn: "O'zbekiston Respublikasi chempioni,Yoshlar orasida yetakchi murabbiy",
-        email: "raximob@gmail.com",
-        telefon: "+99956523565",
-      },
-      {
-        rasm: "https://randomuser.me/api/portraits/men/3.jpg",
-        name: "Asadova Mohinur",
-        lavozim: "Skripkachi",
-        matn: "O'zbekiston Respublikasi chempioni,Yoshlar orasida yetakchi murabbiy",
-        email: "mohinur@gmail.com",
-        telefon: "+99956523565",
-      },
-    ],
+    loading: true,
+    curators: [],
+    curator: null,
     show: false,
+    previewImage: false,
+    imageUrl: "",
+    image: "",
+    courses: [],
+    course: {},
   };
   getCourses = () => {
-    getCourses()
-      .then((res) => 
-      this.setState({loading:false})
-      ) 
-      .catch((err) => console.log(err));
-     
+    getCourses(Global.id)
+      .then((res) => {
+        this.setState({ courses: res.data });
+      })
+      .catch((err) => message.error("Ma'lumot topilmadi"));
+  };
+  getMentors = () => {
+    getStaff().then((res) => {
+      this.setState({ curators: res.data });
+    });
   };
   openModal = () => {
-    this.setState({ show: true }); 
-   
+    this.setState({ show: true });
   };
   handleCancel = () => {
-    this.setState({ show: false });
+    this.setState({ show: false, previewImage: false, edit: null });
+    document.getElementById("title").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("text").value = "";
   };
-  createSubject = () => {
-    console.log("dasdadsa");
+  customRequest = (e) => {
+    this.setState({ previewImage: false, image: e.target.files[0] });
+  };
+  selected = (e) => {
+    this.setState({ curator: e });
+  };
+  editCourses = (key) => {
+    getCourses(Global.id).then((res) => {
+      document.getElementById("title").value = res.data[key].title;
+      document.getElementById("address").value = res.data[key].address;
+      document.getElementById("text").value = res.data[key].text;
+      this.setState({ course: res.data[key], curator: res.data[key].mentor, imageUrl: res.data[key].image, edit: res.data[key].id, previewImage: true });
+    });
+    this.openModal();
+  };
+  saveCourses = () => {
+    var data = {
+      title: document.getElementById("title").value,
+      address: document.getElementById("address").value,
+      text: document.getElementById("text").value,
+      mentor: this.state.curator,
+      school: Global.id,
+    };
+    let formData = new FormData();
+    formData.append("title", data.title ?? null);
+    formData.append("address", data.address ?? null);
+    formData.append("text", data.text ?? null);
+    formData.append("mentor", data.mentor ?? null);
+    formData.append("school", data.school ?? null);
+    if (this.state.edit !== null) {
+      if (this.state.image !== "") {
+        formData.append("image", this.state.image ?? null);
+        editCourse(formData, this.state.edit)
+          .then((res) => {
+            this.getCourses();
+            message.success("Ma'lumot o'zgartirildi");
+          })
+          .catch((err) => message.error("Ma'lumot o'zgartirilmadi"));
+      } else {
+        editCourse(data, this.state.edit)
+          .then((res) => {
+            this.getCourses();
+            message.success("Ma'lumot o'zgartirildi");
+          })
+          .catch((err) => message.error("Ma'lumot o'zgartirilmadi"));
+      }
+    } else {
+      formData.append("image", this.state.image ?? null);
+      createCourses(formData)
+        .then((res) => {
+          this.getCourses();
+          message.success("Ma'lumot yaratildi");
+        })
+        .catch((err) => message.error("Ma'lumot yaratilmadi"));
+    }
+    this.handleCancel();
+  };
+  deleteCourses = (id) => {
+    deleteCourse(id)
+      .then((res) => {
+        this.getCourses();
+        message.success("Ma'lumot o'chirildi");
+      })
+      .catch((err) => message.error("Ma'lumot o'chirilmadi"));
   };
   componentDidMount() {
     this.getCourses();
-   
+    this.getMentors();
+    this.setState({ loading: false });
   }
   render() {
     return (
-      <div>{this.state.loading===true?(<Loader/>):( 
       <div>
-        <h1 style={{ fontSize: "30px", display: "inline-block", marginRight: "30px" }}>
-          To'garaklar{" "}
-          <Button onClick={this.openModal} style={{ marginLeft: "30px", backgroundColor: "#187CC0", border: "none" }}>
-            Qo'shish
-          </Button>
-        </h1>
-        <Row>
-          {this.state.teachers.map((item, key) => {
-            return (
-              <Col lg={4} md={6} sm={12} style={{ marginTop: "20px" }}>
-                <div className={styles.card}>
-                  <div className={(styles.card, styles.cardone)}>
-                    <header>
-                      <div className={styles.avatar}>
-                        <img src={item.image !== null ? item.image : admin} alt="" />
-                      </div>
-                    </header>
+        {this.state.loading === true ? (
+          <Loader />
+        ) : (
+          <div>
+            <h1 style={{ fontSize: "30px", display: "inline-block", marginRight: "30px" }}>
+              To'garaklar{" "}
+              <Button onClick={this.openModal} style={{ marginLeft: "30px", backgroundColor: "#187CC0", border: "none" }}>
+                Qo'shish
+              </Button>
+            </h1>
+            <Row>
+              {this.state.courses !== []
+                ? this.state.courses.map((item, key) => {
+                    return (
+                      <Col lg={4} md={6} sm={12} style={{ marginTop: "20px" }}>
+                        <div className={styles.card}>
+                          <div className={(styles.card, styles.cardone)}>
+                            <header>
+                              <div className={styles.avatar}>
+                                <img src={item.image !== null ? item.image : admin} alt="" />
+                              </div>
+                            </header>
 
-                    <h3 className={styles.headerName}>{item.name}</h3>
-                    <div className={styles.desc}>
-                      <p>{item.title}</p>
-                      <p>{item.mentor}</p>
-                      <p style={{ marginTop: "-40px", fontWeight: "800" }}>{item.address}</p>
-                      <p style={{ marginTop: "-40px", fontWeight: "800" }}>{item.text}</p>
-                    </div>
+                            <h3 className={styles.headerName}>{item.title}</h3>
+                            <div className={styles.desc}>
+                              <p>O'qituvchi: {item.mentor}</p>
+                              <p style={{ marginTop: "-40px", fontWeight: "800" }}>Manzili: {item.address}</p>
+                              <p style={{ marginTop: "-40px", fontWeight: "800" }}>Qo'shimcha ma'lumot: {item.text}</p>
+                            </div>
 
-                    <footer className={styles.footer} style={{ marginTop: "-40px", fontWeight: "800" }}>
-                      <FaEdit style={{ color: "green", fontSize: "20px" }} onClick={() => this.editTeacher1(key)} />
-                      <MdDelete style={{ color: "red", fontSize: "20px", marginLeft: "20px" }} onClick={() => this.deleteTeacher1(key)} />
-                    </footer>
-                  </div>
-                </div>
-              </Col>
-            );
-          })}
-        </Row>
-        <Modal title="Sinf yaratish" visible={this.state.show} onCancel={this.handleCancel} footer={false}>
-          <Form>
-            {/* <Form.Group className="mb-3" controlId="curator">
-                            <Form.Label>Sinf rahbari</Form.Label>
-                            <Select style={{ width: "100%" }} defaultValue={this.state.curator} onChange={this.selected} optionLabelProp="label">
-                                {this.state.curators !== null
-                                    ? this.state.curators.map((item) => {
-                                          console.log(item.id);
-                                          return (
-                                              <Option value={item.id} label={item.full_name}>
-                                                  {item.full_name}
-                                              </Option>
-                                          );
-                                      })
-                                    : ""}
-                            </Select>
-                        </Form.Group> */}
+                            <footer className={styles.footer} style={{ marginTop: "-40px", fontWeight: "800" }}>
+                              <FaEdit style={{ color: "green", fontSize: "20px" }} onClick={() => this.editCourses(key)} />
+                              <MdDelete style={{ color: "red", fontSize: "20px", marginLeft: "20px" }} onClick={() => this.deleteCourses(item.id)} />
+                            </footer>
+                          </div>
+                        </div>
+                      </Col>
+                    );
+                  })
+                : ""}
+            </Row>
+            <Modal title="To'garak yaratish" visible={this.state.show} onCancel={this.handleCancel} footer={false}>
+              <Form>
+                <Form.Group className="mb-3" controlId="title">
+                  <Form.Label>To'garak nomi</Form.Label>
+                  <Form.Control placeholder="To'garak nomi" defaultValue={this.state.course.title} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="mentor">
+                  <Form.Label>To'garak rahbari</Form.Label>
+                  <Select style={{ width: "100%" }} defaultValue={this.state.curator} onChange={this.selected} optionLabelProp="label">
+                    {this.state.curators !== null
+                      ? this.state.curators.map((item) => {
+                          return (
+                            <Option value={item.id} label={item.full_name}>
+                              {item.full_name}
+                            </Option>
+                          );
+                        })
+                      : ""}
+                  </Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="image">
+                  <Form.Label>To'garak rasmi</Form.Label>
+                  <br />
+                  <Form.Control className="formInput" accept=".jpg, .jpeg, .png" onChange={this.customRequest} name="image" required type="file" />
+                  <br />
+                  <br />
+                  {this.state.previewImage ? ImageDemo(this.state.imageUrl) : ""}
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="classNumber">
-              <Form.Label>Sinf id</Form.Label>
-              <Form.Control className="formInput" type="number" min="1" max="11" placeholder="1" />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="address">
+                  <Form.Label>To'garak manzili</Form.Label>
+                  <Form.Control placeholder="To'garak manzili" defaultValue={this.state.course.address} />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="classChar">
-              <Form.Label>Sinf</Form.Label>
-              <Form.Control className="formInput" type="text" placeholder="A" pattern="[A-Z]{1}" />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="text">
+                  <Form.Label>To'garak matni</Form.Label>
+                  <Form.Control as="textarea" defaultValue={this.state.course.text} style={{ height: "120px" }} placeholder="To'garak matni" />
+                </Form.Group>
 
-            <br />
+                <br />
 
-            <Button type="danger" htmlType="button" style={{ marginRight: "10px" }} onClick={this.handleCancel}>
-              Bekor qilish
-            </Button>
-            <Button type="primary" htmlType="button" onClick={this.createSubject}>
-              Saqlash
-            </Button>
-          </Form>
-        </Modal>
-        </div>)}
+                <Button variant="danger" style={{ marginRight: "10px" }} onClick={this.handleCancel}>
+                  Bekor qilish
+                </Button>
+                <Button variant="primary" onClick={this.saveCourses}>
+                  Saqlash
+                </Button>
+              </Form>
+            </Modal>
+          </div>
+        )}
       </div>
     );
   }
